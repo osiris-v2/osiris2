b955ba7f5d0db00dbc73259b3045ef852d5c15b5
root <root@osiris.osiris>
2025-05-28 05:24:24 +0200

    UFL 2025-05-28_05-24-23

--- bin/scripts/files2deb.sh
+++ bin/scripts/files2deb.sh
@@ -5,7 +5,6 @@ cd $script_dir
 #este script incorpora a la carpeta DEBIAN 
 #los archivos de entorno global de osiris
 
-
 DIST="osiris2"
 USRBIN="/usr/bin"
 USRBIO="/usr/local/${DIST}/bio"
@@ -15,3 +14,4 @@ BASEBIO="${DEBIAN}${USRBIO}"
 cp $USRBIN/osiris2tmux  ${BASEBIO}/osiris2tmux
 cp $USRBIN/o2load  ${BASEBIO}/o2load
 
+cp $USRBIO/release/bio.deb $BASEBIO/release/bio.deb
