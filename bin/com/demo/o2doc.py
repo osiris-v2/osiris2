d126959d81053587596134c4d11611a26601780d
root <root@osiris.osiris>
2025-05-28 08:20:01 +0200

    UFL 2025-05-28_08-20-01

--- bin/com/demo/o2doc.py
+++ bin/com/demo/o2doc.py
@@ -3,6 +3,7 @@ import os
 import subprocess
 from jinja2 import Environment, FileSystemLoader
 import markdown # Para convertir Markdown a HTML para la visualización en Qt
+import sys
 
 # Importar los módulos de PyQt5
 from PyQt5.QtWidgets import (
